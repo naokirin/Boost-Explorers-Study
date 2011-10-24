b83b6970ca6be1c11eca63fa1d57bb67b3ff4fb5
naokirin <malfunctional-ai@sings.jp>
2011-10-24 10:27:17 +0900

    int型を保持できるQueueクラスを作成

---
+++ naokirin/1st/src/queue.hpp
@@ -0,0 +1,39 @@
+#ifndef __QUEUE_HPP
+#define __QUEUE_HPP
+
+#include <list>
+
+namespace rinsin{
+
+  class Queue {
+    std::list<int> data;
+
+  public:
+
+    Queue() = default;
+
+    Queue(const std::list<int> l){
+      data = l;
+    }
+
+    void push(const int& value){
+      data.push_back(value);
+    }
+
+    void pop(){
+      data.pop_front();
+    }
+
+    int front() const{
+      return data.front();
+    }
+
+    std::list<int> getData(){
+      return data;
+    }
+  
+  };
+}
+
+#endif // __QUEUE_HPP
+
---
+++ naokirin/1st/test/queue_test.cpp
@@ -0,0 +1,56 @@
+#include "../src/queue.hpp"
+#include <list>
+#include <memory>
+#include <gtest/gtest.h>
+
+using namespace rinsin;
+
+class QueueTest : public ::testing::Test 
+{
+protected:
+  std::unique_ptr<Queue> queue;
+
+  virtual void SetUp() {
+    queue = std::unique_ptr<Queue>(new Queue());
+  }
+
+  virtual void TearDown() {}
+};
+
+// Test Queue.push()
+TEST_F(QueueTest, testPush){
+  queue->push(1);
+  EXPECT_EQ(1, queue->front());
+  EXPECT_EQ((size_t)1, queue->getData().size());
+
+  queue->push(2);
+  EXPECT_EQ(1, queue->front());
+  EXPECT_EQ((size_t)2, queue->getData().size());
+}
+
+// Test Queue.pop()
+TEST_F(QueueTest, testPop){
+  queue->push(1);
+  queue->push(2);
+  queue->push(3);
+  queue->pop();
+  EXPECT_EQ(2, queue->front());
+  EXPECT_EQ((size_t)2, queue->getData().size());
+
+  queue->pop();
+  EXPECT_EQ(3, queue->front());
+  EXPECT_EQ((size_t)1, queue->getData().size());
+
+  queue->pop();
+  EXPECT_TRUE(queue->getData().empty());
+  EXPECT_EQ((size_t)0, queue->getData().size());
+}
+
+// Test Queue Constructor
+TEST_F(QueueTest, testConstructor){
+  queue = std::unique_ptr<Queue>(new Queue(std::list<int>(3, 2)));
+  EXPECT_TRUE(std::list<int>(3, 2) == queue->getData());
+
+  queue = std::unique_ptr<Queue>(new Queue(std::list<int>(0)));
+  EXPECT_TRUE(std::list<int>(0) == queue->getData());
+}
