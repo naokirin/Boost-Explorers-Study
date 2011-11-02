804b1f78eede51e860704a01be42677fce5e26dc
naokirin <malfunctional-ai@sings.jp>
2011-11-03 03:15:36 +0900

    C++11のstatic_assertのサンプルコードを削除

--- naokirin/utility/static_assert/src/cpp11_static_assert.hpp
+++
@@ -1,7 +0,0 @@
-#include <cassert>
-#include <boost/type_traits.hpp>
-
-template <class T>
-void trivial_ctor_checker(){
-  static_assert(boost::has_trivial_constructor<T>::value, "Not has trivial Ctor!");
-};
--- naokirin/utility/static_assert/src/static_assert_main.cpp
+++ naokirin/utility/static_assert/src/static_assert_main.cpp
@@ -1,12 +1,6 @@
 #include <vector>
 #include "static_assert_class.hpp"
 #include "static_assert_function.hpp"
-#include "cpp11_static_assert.hpp"
-
-class Foo{Foo() = default;};
-class Bar{};
-class Far{Far(){};};
-class Boo{Boo(int){}};
 
 int main(){
   // static_assert_class.hpp
@@ -20,12 +14,6 @@ int main(){
   upto10<1>(); // OK
   upto10<5>(); // OK
   // upto10<10>(); // Compile Error!
-
-  // cpp11_static_assert.hpp
-  trivial_ctor_checker<Foo>(); // OK
-  trivial_ctor_checker<Bar>(); // OK
-  // trivial_ctor_checker<Far>(); // Compile Error!
-  // trivial_ctor_checker<Boo>(); // Compile Error!
 
   return 0;
 }
