0a4ac5daeba35152088c006334f4a7ae9ecfb46c
naokirin <malfunctional-ai@sings.jp>
2011-11-05 21:54:58 +0900

    型推論のサンプルコードを修正

--- naokirin/cpp11/type_inference/src/type_inference_main.cpp
+++ naokirin/cpp11/type_inference/src/type_inference_main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <complex>
 
+// T型とU型のデータを*で演算したときの
+// 戻り値の型が戻り値になる
 template <class T, class U>
 auto test(T a, U b) -> decltype(a*b){
 
@@ -13,12 +16,15 @@ int main(){
   for(int i =0; i < 10; ++i)
     vec.push_back(i);
 
+  // イテレータの型の宣言は長くなりがちなので、
+  // autoで型推論してもらう
   for(auto itr=vec.begin(); itr != vec.end(); ++itr)
     std::cout << *itr;
 
   std::cout << std::endl;
 
-  std::cout << test(1.0, 2.0) << std::endl;
+  auto z = std::complex<int>(1, 2);
+  std::cout << test(z, 2) << std::endl;
 
   return 0;
 }
